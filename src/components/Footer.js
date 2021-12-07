651ae940561cbac441019356e1ee2ccdcf6ed9ca
Kuhl <milsapmichael@gmail.com>
2021-12-07 01:21:58 -0800

    removed image tag

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,12 +1,7 @@
-import { fab } from "@fortawesome/free-brands-svg-icons";
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import React from "react";
 
 export const Footer = () => (
   <footer>
-    <a href='https://github.com/mikekuhl'>
-      <FontAwesomeIcon icon={[fab, "github"]} />
-      Made By Michael Milsap
-    </a>
+    <a href='https://github.com/mikekuhl'>Made By Michael Milsap</a>
   </footer>
 );
